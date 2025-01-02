b936657ce20fb4a5c71633854e155251fc0e2b45
jis24 <165779138+orscoder@users.noreply.github.com>
2025-01-03 00:32:32 +0530

    frame is made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+
+    <section className="padding">Popular product</section>
+    <section className="padding">Super Quality</section>
+    <section className="py-10 padding-x">Services</section>
+    <section className="padding">Special offers</section>
+    <section className="bg-pale-blue padding">customer reviews</section>
+    <section className="w-full py-16 padding-x sm:py-32">Subscribe</section>
+    <section className="pb-8 bg-black padding-x padding-t">Footer</section>
+  </main>
+);
+
+export default App;
