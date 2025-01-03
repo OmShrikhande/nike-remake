06136a95e9ee4657155ca119e88b6d5eb26d963c
jis24 <165779138+orscoder@users.noreply.github.com>
2025-01-03 15:27:17 +0530

    components done
    components are cerated and added to the src/App.jsx this is the basic structure creation of the code and this will be updated as we work on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { Hero,
+  CustomerReview,
+  Footer,
+  Subscribe,
+  Nav,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+ } from "./sections";
 const App = () => (
   <main className="relative">
-    Nav
+    
     <section className="xl:padding-1 wide:padding-r padding-b">
       Hero
     </section>
 
-    <section className="padding">Popular product</section>
-    <section className="padding">Super Quality</section>
-    <section className="py-10 padding-x">Services</section>
-    <section className="padding">Special offers</section>
-    <section className="bg-pale-blue padding">customer reviews</section>
-    <section className="w-full py-16 padding-x sm:py-32">Subscribe</section>
-    <section className="pb-8 bg-black padding-x padding-t">Footer</section>
+    <section className="padding"><PopularProducts/></section>
+    <section className="padding"><SuperQuality/></section>
+    <section className="py-10 padding-x"><Services/></section>
+    <section className="padding"><SpecialOffer/></section>
+    <section className="bg-pale-blue padding"><CustomerReview/></section>
+    <section className="w-full py-16 padding-x sm:py-32"><Subscribe/></section>
+    <section className="pb-8 bg-black padding-x padding-t"><Footer /></section>
   </main>
 );
 
---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import Nav from "./Nav";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    CustomerReview,
+    Footer,
+    Subscribe,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    // Add more components here as needed...
+ };
