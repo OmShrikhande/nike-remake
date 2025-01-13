c0b40ec9d2c10b33e31ad6d064a851a6eb3c2f16
jis24 <165779138+orscoder@users.noreply.github.com>
2025-01-13 23:53:01 +0530

    added button

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const Button = ({label,iconUrl}) => {
   return (
     <Button className="">
-        {label}
-
-        <img src={iconUrl} alt="arrow right icon" className="w-5 h-5 ml-2 rounded-full "/>
+      
     </Button>
   )
 }
-
 export default Button
